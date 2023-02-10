b88becab6b17ef735a5ca68094bc41db11f7735e
Coffe818 <josemanuelgrimaldovalero@gmail.com>
2023-02-10 16:37:07 -0600

    Agrege metodo eliminar y se utiliza JOptionPanel

--- Tareas/ColasDinamicas.java
+++ Tareas/ColasDinamicas.java
@@ -1,13 +1,23 @@
 package Tareas;
  import java.util.LinkedList;
+ import javax.swing.JOptionPane;
 public class ColasDinamicas {
     static LinkedList <Integer> cola = new LinkedList<Integer>();
     
     private boolean Vacia() {
         return cola.isEmpty();
-    }
+    }//end vacia
 
-    private void Insertar( int x) {
-        cola.add(x);
-    }
+    private void Insertar( int elemento) {
+        cola.add(elemento);
+    }//end Insertar
+
+    private void Eliminar() {
+        if (!Vacia()) {
+            int elemento =cola.poll();
+            JOptionPane.showMessageDialog(null, "Se elimno el elemento: "+elemento);
+        }else {
+            JOptionPane.showMessageDialog(null, "La cola esta vacia");
+        }//end if else
+    }//end eliminar
 }//ens class
