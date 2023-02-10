76b147222991d76e8533cdc4852e39bfe3f8c762
Coffe818 <josemanuelgrimaldovalero@gmail.com>
2023-02-10 16:26:42 -0600

    Se agrega el metodo vacia

--- Tareas/ColasDinamicas.java
+++ Tareas/ColasDinamicas.java
@@ -2,4 +2,8 @@ package Tareas;
  import java.util.LinkedList;
 public class ColasDinamicas {
     static LinkedList <Integer> cola = new LinkedList<Integer>();
+    
+    private boolean Vacia() {
+        return cola.isEmpty();
+    }
 }//ens class
