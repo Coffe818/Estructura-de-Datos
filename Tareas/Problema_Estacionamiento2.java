2cf903fab7fb0a6c5e1defcaf9e88459b460c688
Coffe818 <josemanuelgrimaldovalero@gmail.com>
2023-02-14 14:48:53 -0600

    agregue, Vaciar, Imprimir y eliminarlos Vehiculos

--- Tareas/Problema_Estacionamiento2.java
+++ Tareas/Problema_Estacionamiento2.java
@@ -15,5 +15,33 @@ public class Problema_Estacionamiento2 {
         pilaTemporal = new Stack<>();
     }// end constructor
 
+    private boolean Vacia() {
+        return colaPrincipal.isEmpty();
+    }// end vacia
+
+    public void insertarVehiculo(String placa) {
+        placa = placa.trim();// elimiana los espacios a la inicio y final de la palabra
+        colaPrincipal.push(placa);
+    }// end insertar Vehiculo
+
+    public void eliminarVehiculo(String placa) {
+        if (Vacia()) {
+            JOptionPane.showInputDialog("La cola esta vacia");
+        } else {
+            if (colaPrincipal.peek().equals(placa)) {
+                colaPrincipal.pop();
+            } else {
+                while (!colaPrincipal.peek().equals(placa)) {
+                    pilaTemporal.push(colaPrincipal.pop());
+                } // end while
+                colaPrincipal.pop();
+                while (!pilaTemporal.empty()) {
+                    colaPrincipal.push(pilaTemporal.pop());
+                } // end while
+                JOptionPane.showMessageDialog(null, "Se eliminó el vehículo: " + placa);
+            } // end if else
+        } // end if else
+    }// end elimiar placa
+
 
 }// end class
