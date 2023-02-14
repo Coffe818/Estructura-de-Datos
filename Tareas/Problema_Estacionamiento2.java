bb35a3c93a2170879b52da1083c483ff93882593
Coffe818 <josemanuelgrimaldovalero@gmail.com>
2023-02-14 14:47:45 -0600

    Cree la clase, con la cola y pila y su constructor

---
+++ Tareas/Problema_Estacionamiento2.java
@@ -0,0 +1,19 @@
+package Tareas;
+
+import java.util.LinkedList;
+import java.util.Stack;
+
+import javax.swing.JOptionPane;
+
+public class Problema_Estacionamiento2 {
+    LinkedList<String> colaPrincipal;
+    Stack<String> pilaTemporal;
+
+    // contructor
+    public Problema_Estacionamiento2() {
+        colaPrincipal = new LinkedList<>();
+        pilaTemporal = new Stack<>();
+    }// end constructor
+
+
+}// end class
