fe0a8a3f849e0fee22b15b350237f3698f57b5b9
Coffe818 <josemanuelgrimaldovalero@gmail.com>
2023-02-22 19:23:04 -0600

    Mejor identado, sigo con error en case 3

--- Tareas/ListaEncadenadaSimple.java
+++ Tareas/ListaEncadenadaSimple.java
@@ -24,7 +24,7 @@ public class ListaEncadenadaSimple {
     Nodo ant;
     Nodo temp;
     Nodo nuevoNodo;
-    
+
     public ListaEncadenadaSimple() {
         this.first = null;
         this.ant = null;
@@ -38,156 +38,160 @@ public class ListaEncadenadaSimple {
 
     public void Insertar(int numEmp, String nombre, int depto, float sueldo) {
         nuevoNodo = new Nodo(numEmp, nombre, depto, sueldo);
-        boolean repite =true;
-        while (temp != null && temp.numEmp <= numEmp) {//recorre y compara los numEmp para que no se repitan
-                        ant = temp;
-                        temp = temp.next;
-                    
+        boolean repite = true;
+        while (temp != null && temp.numEmp <= numEmp) {// recorre y compara los numEmp para que no se repitan
+            ant = temp;
+            temp = temp.next;
+
             if (temp.numEmp == numEmp) {
                 System.out.println("No se puede repetir numero de empleado");
-                repite=true;
-            }else{
-                repite=false;
+                repite = true;
+            } else {
+                repite = false;
             }
-        }//end while
+        } // end while
 
-            if (!repite) {
-                if (Vacia()) {
+        if (!repite) {
+            if (Vacia()) {
+                first = nuevoNodo;
+            } else {
+                temp = first;
+                ant = temp;
+
+                while (temp != null && temp.depto < depto) {// aqui es un while que recorre el temp y el ant hasta
+                                                            // que el
+                                                            // departamento sea menor que el que se esta insertando
+                    ant = temp;
+                    temp = temp.next;
+                } // end while para depto
+
+                while (temp != null && temp.depto == depto && temp.numEmp < numEmp) {// una vez ya ordenado por
+                                                                                     // departametno
+                                                                                     // este lo recorre por numero
+                                                                                     // de
+                                                                                     // elempleado
+                    ant = temp;
+                    temp = temp.next;
+                } // end while para numEmp
+
+                if (temp == first) {
+                    nuevoNodo.next = first;
                     first = nuevoNodo;
                 } else {
-                    temp = first;
-                    ant = temp;
-
-                    while (temp != null && temp.depto < depto) {// aqui es un while que recorre el temp y el ant hasta
-                                                                // que el
-                                                                // departamento sea menor que el que se esta insertando
-                        ant = temp;
-                        temp = temp.next;
-                    } // end while para depto
-
-                    while (temp != null && temp.depto == depto && temp.numEmp < numEmp) {// una vez ya ordenado por
-                                                                                         // departametno
-                                                                                         // este lo recorre por numero
-                                                                                         // de
-                                                                                         // elempleado
-                        ant = temp;
-                        temp = temp.next;
-                    } // end while para numEmp
-
-                    if (temp == first) {
-                        nuevoNodo.next = first;
-                        first = nuevoNodo;
-                    } else {
-                        nuevoNodo.next = ant.next;
-                        ant.next = nuevoNodo;
-                    }//end if else que agrega los datos
-                } // end if else que contirne los while
-            }//end if donde se valida qeu se repita el numero de empleado           
+                    nuevoNodo.next = ant.next;
+                    ant.next = nuevoNodo;
+                } // end if else que agrega los datos
+            } // end if else que contirne los while
+        } // end if donde se valida qeu se repita el numero de empleado
     }// end Insertar
 
     public void Borrar(int numEmp) {
         if (!Vacia()) {
             temp = first;
             ant = null;
-            
+
             while (temp != null && temp.numEmp != numEmp) {
                 ant = temp;
                 temp = temp.next;
-            }//end while que recorre el numEmp siempre y que sea diferente al que queramos borrar
-            
+            } // end while que recorre el numEmp siempre y que sea diferente al que queramos
+              // borrar
+
             if (temp != null) {
                 if (ant == null) {
-                    first = temp.next; //en caso de que sea el primero                    
+                    first = temp.next; // en caso de que sea el primero
                 } else {
-                    ant.next = temp.next;                     
-                }//end if else para borrar el elemento dependiendo del caso
-                System.out.println("Se ha borrado el empleado numero: "+ numEmp);
-            }// end if para borrar elemento
-        }else{
+                    ant.next = temp.next;
+                } // end if else para borrar el elemento dependiendo del caso
+                System.out.println("Se ha borrado el empleado numero: " + numEmp);
+            } // end if para borrar elemento
+        } else {
             System.out.println("Esta vacia la lista");
-        }//end if else que verifica si esta vacia o no
-    }//end Borrar
+        } // end if else que verifica si esta vacia o no
+    }// end Borrar
 
     public void ImprimirTodo() {
         if (!Vacia()) {
             temp = first;
-            
+
             while (temp != null) {
                 System.out.println(temp.numEmp + "\t" + temp.nombre + "\t" + temp.depto + "\t" + temp.sueldo);
                 temp = temp.next;
-            }//end while para imprimir
-        }else{
+            } // end while para imprimir
+        } else {
             System.out.println("La lista esta vacia");
-        }//end if else
-    }//en imprimir todo
+        } // end if else
+    }// en imprimir todo
 
     public void ImprimirPorDepto(int depto) {
         if (!Vacia()) {
             temp = first;
-            
+
             while (temp != null) {
                 if (temp.depto == depto) {
                     System.out.println(temp.numEmp + "\t" + temp.nombre + "\t" + temp.depto + "\t" + temp.sueldo);
-                }// end if para imprimir al empleado 
-                
+                } // end if para imprimir al empleado
+
                 temp = temp.next;
-            }//end while que recorre siempre e imprime siempre que sea el depto correspondiente
-        }//end if
-    }//end Imprimir Por Depto
-    
+            } // end while que recorre siempre e imprime siempre que sea el depto
+              // correspondiente
+        } // end if
+    }// end Imprimir Por Depto
+
     public void ImprimirPrimerElemento() {
         if (!Vacia()) {
             System.out.println(first.numEmp + "\t" + first.nombre + "\t" + first.depto + "\t" + first.sueldo);
-        }else{
+        } else {
             System.out.println("La pila esta vacia");
-        }// end if else
-    }//end Imprimir primer elemento
+        } // end if else
+    }// end Imprimir primer elemento
 
     public void Tamano() {
         int contador = 0;
-        
+
         if (!Vacia()) {
             temp = first;
-            
+
             while (temp != null) {
                 contador++;
                 temp = temp.next;
-            }//end while
-        }//end if
-        
-        System.out.println("La cantidad de epleados es de: "+contador);
-    }//end Tamano
+            } // end while
+        } // end if
+
+        System.out.println("La cantidad de epleados es de: " + contador);
+    }// end Tamano
 
     public void ImprimirPorSueldo(double xSal) {
         temp = first;
         while (temp != null) {
             if (temp.sueldo >= xSal) {
-                System.out.println("NumEmp: " + temp.numEmp + ", Nombre: " + temp.nombre + ", Depto: " + temp.depto + ", Sueldo: " + temp.sueldo);
-            }//end if
+                System.out.println("NumEmp: " + temp.numEmp + ", Nombre: " + temp.nombre + ", Depto: " + temp.depto
+                        + ", Sueldo: " + temp.sueldo);
+            } // end if
             temp = temp.next;
-        }//end while
-    }//end Imprimir por sueldo
+        } // end while
+    }// end Imprimir por sueldo
 
     public void Invertir() {
-            ant = null;
-            temp = first;
-            first = null;
-            while (temp != null) {
-                ant = temp;
-                temp = temp.next;
-                ant.next = first;
-                first = ant;
-            }// end while que recorre e invierte la direccion de los apuntadores, para asi invertirlos
-    }//end Invertir
-        
-    //main
+        ant = null;
+        temp = first;
+        first = null;
+        while (temp != null) {
+            ant = temp;
+            temp = temp.next;
+            ant.next = first;
+            first = ant;
+        } // end while que recorre e invierte la direccion de los apuntadores, para asi
+          // invertirlos
+    }// end Invertir
+
+    // main
     public static void main(String[] args) {
         ListaEncadenadaSimple lista = new ListaEncadenadaSimple();
         Scanner scanner = new Scanner(System.in);
         int opcion;
-        
+
         do {
-            System.out.println("====== MENÚ LISTA ENLAZADA SIMPLE ======");
+            System.out.println("====== MENU LISTA SIMPLE ======");
             System.out.println("1. Insertar elemento ordenado por número de empleado");
             System.out.println("2. Borrar elemento por número de empleado");
             System.out.println("3. Imprimir todos los elementos de la lista");
@@ -200,7 +204,7 @@ public class ListaEncadenadaSimple {
             System.out.print("Ingrese opción: ");
             opcion = scanner.nextInt();
             scanner.nextLine();
-            
+
             switch (opcion) {
                 case 1:
                     System.out.print("Ingrese número de empleado: ");
@@ -213,7 +217,9 @@ public class ListaEncadenadaSimple {
                     System.out.print("Ingrese sueldo: ");
                     float sueldo = scanner.nextFloat();
 
-                    lista.Insertar(numemp, nombre, depto, sueldo);
+                    Nodo nodo = lista.new Nodo(numemp, nombre, depto, sueldo);
+                    lista.Insertar(nodo);
+
                     break;
                 case 2:
                     System.out.print("Ingrese número de empleado a borrar: ");
@@ -249,10 +255,10 @@ public class ListaEncadenadaSimple {
                 default:
                     System.out.println("Opción inválida");
                     break;
-            }//end switch
+            }// end switch
             System.out.println();
         } while (opcion != 0);
         scanner.close();
-    }//end main
-    
+    }// end main
+
 }// end class
