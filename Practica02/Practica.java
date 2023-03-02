2d2ee5e61a35c9d4d528b875994818fba3db5c44
Coffe818 <josemanuelgrimaldovalero@gmail.com>
2023-03-02 08:08:14 -0600

    BorrarColaInsertarPila

--- Practica02/Practica.java
+++ Practica02/Practica.java
@@ -8,6 +8,7 @@ public class Practica {
     private Stack<Integer> PilaTemp = new Stack<Integer>();
     private LinkedList<Integer> Cola1 = new LinkedList<Integer>();
     private LinkedList<Integer> Cola2 = new LinkedList<Integer>();
+    private int elemento = 0;
 
     // los metodos vacia reciven una pila o una cola y luego verifica si esta vacia,
     // esto para ahorrar escribir un metodo Vacia(), para cada pila y cada cola
@@ -19,31 +20,41 @@ public class Practica {
         return pila.isEmpty();
     }// end Vacia Pila
 
-    //Los metodos insertar tienen la misma logica que Vacia() solo que recibe un elemto para insertarlo
-    private void InsertarCola(LinkedList<Integer> cola, int elemento) {
-        cola.add(elemento);
+    // Los metodos insertar tienen la misma logica que Vacia() solo que recibe un
+    // elemto para insertarlo
+    private void InsertarCola(LinkedList<Integer> cola, int xelem) {
+        cola.add(xelem);
     }// end Insertar Cola
-    private void InsertarPila(Stack<Integer> pila, int elemento){
-        pila.push(elemento);
+
+    private void InsertarPila(Stack<Integer> pila, int xelem) {
+        pila.push(xelem);
     }// end Insertar Pila
 
-    //lso metod insertar piden un linkedlist o un Stack para saber en cual de las dos colas o cual de las dos pilas elimina el elemento
+    // lso metod insertar piden un linkedlist o un Stack para saber en cual de las
+    // dos colas o cual de las dos pilas elimina el elemento y lo guarda en una
+    // variable
     private void BorrarCola(LinkedList<Integer> cola) {
         if (!VaciaCola(cola)) {
-            int elemento = cola.poll();
-            System.out.println("Se elimino cliente: "+ elemento);
+            this.elemento = cola.poll();
+            System.out.println("Se elimino cliente: " + elemento);
         } else {
             System.out.println("La cola esta vacia");
-        }//end if else
+        } // end if else
     }// end Borrar Cola
+
     private void BorrarPila(Stack<Integer> pila) {
         if (!VaciaPila(pila)) {
-            int elemento = pila.peek();
+            this.elemento = pila.peek();
             pila.pop();
             System.out.println("Elemento eliminado: " + elemento);
         } else {
             System.out.println("La pila está vacía");
         } // end if else
-    }//end Borrar Pila
+    }// end Borrar Pila
+
+    private void BorrarColaInsertarPila() {
+        BorrarCola(Cola1);
+        InsertarPila(Pila, elemento);
+    }// end borrar e insertar
 
 }// end clas
