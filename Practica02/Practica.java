ab6f9c844d50193035c91c92290c06a93a5d47bd
Coffe818 <josemanuelgrimaldovalero@gmail.com>
2023-03-02 07:32:46 -0600

    Se declaran las pilas y colas

--- Practica02/Practica.java
+++ Practica02/Practica.java
@@ -1,6 +1,14 @@
 package Practica02;
 
+import java.util.LinkedList;
+import java.util.Stack;
+
 public class Practica {
+    private Stack<Integer> Pila = new Stack<Integer>();
+    private Stack<Integer> PilaTemp = new Stack<Integer>();
+    static LinkedList <Integer> Cola1 = new LinkedList<Integer>();
+    static LinkedList <Integer> Cola2 = new LinkedList<Integer>();
     
+
     
 }// end clas
