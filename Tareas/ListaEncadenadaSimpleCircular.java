5afcaa0aa5325ebceb619bb1566abb02016d33eb
Coffe818 <josemanuelgrimaldovalero@gmail.com>
2023-03-06 18:48:06 -0600

    class Nodo

---
+++ Tareas/ListaEncadenadaSimpleCircular.java
@@ -0,0 +1,24 @@
+package Tareas;
+
+public class ListaEncadenadaSimpleCircular {
+    
+    public class Nodo {
+        int numEmp;
+        String nombre;
+        int depto;
+        float sueldo;
+        Nodo next;
+
+        public Nodo(int numEmp, String nombre, int depto, float sueldo) {
+            this.numEmp = numEmp;
+            this.nombre = nombre;
+            this.depto = depto;
+            this.sueldo = sueldo;
+            this.next = null;
+        }// end constructor del nodo
+    }// end class nodo
+
+    Nodo ant;
+    Nodo temp;
+    Nodo nuevoNodo;
+}//end class
