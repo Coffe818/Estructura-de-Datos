4e2bbbe142b7e12b67d41530e8040fa890faeeea
Coffe818 <josemanuelgrimaldovalero@gmail.com>
2023-03-06 19:02:48 -0600

    Creo que voy bien (?

--- Tareas/ListaEncadenadaSimpleCircular.java
+++ Tareas/ListaEncadenadaSimpleCircular.java
@@ -21,4 +21,42 @@ public class ListaEncadenadaSimpleCircular {
     Nodo ant;
     Nodo temp;
     Nodo nuevoNodo;
+    Nodo nc;
+
+    public void AgregarNC(){
+        nuevoNodo = new Nodo(0, null, 0, 0);
+        nc=nuevoNodo;
+    }//end AgregarNC
+
+    public boolean Vacia(){
+        return temp==nc;
+    }//end Vacia
+
+    public void Insertar(int numEmp, String nombre, int depto, float sueldo) {
+        nuevoNodo = new Nodo(numEmp, nombre, depto, sueldo);
+        if (Vacia()) {
+            nc.next = nuevoNodo;
+            temp=nuevoNodo;
+        } else {
+            temp=nc;
+            ant=nc;
+            while (temp != null && temp.depto <= depto && temp.numEmp < numEmp) {// Recore ant y temp
+                                                                                 // hasta que se acomode
+                                                                                 // primero por departamento
+                                                                                 // y luego por
+                                                                                 // num empleado
+                ant = temp;
+                temp = temp.next;
+            } // end while para numEmp
+
+            if (ant == nc) {
+                temp.next = nuevoNodo;
+            } else {
+                ant.next = nuevoNodo;
+            } // end if else que agrega los datos
+            nuevoNodo.next = temp;
+        } // end if else que contirne los while
+    }//end insertar
+
+
 }//end class
