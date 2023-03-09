2781566bcfba2469b90e88d6c1aeb0f78c809e2b
Coffe818 <josemanuelgrimaldovalero@gmail.com>
2023-03-09 07:35:21 -0600

    declare la clase nodo

--- Practica02/ExamenParcial1.java
+++ Practica02/ExamenParcial1.java
@@ -1,5 +1,24 @@
 package Practica02;
 
+import java.util.LinkedList;
+import java.util.Stack;
+
 public class ExamenParcial1 {
+    private static Stack<Integer> Pila = new Stack<Integer>();
+    private static LinkedList<Integer> Cola1 = new LinkedList<Integer>();
+
+    public class Nodo {
+        int numcle;
+        Nodo next;
+        public Nodo(int numcle) {
+            this.numcle = numcle;
+            this.next = null;
+        }// end constructor del nodo
+    }// end class nodo
+
+    Nodo inicio;
+    Nodo fin;
+    Nodo temp;
+    Nodo ant;
     
 }//end class
