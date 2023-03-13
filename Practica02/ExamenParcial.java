d3432371d11a1d364ef8417a154c4fcd99462126
Coffe818 <josemanuelgrimaldovalero@gmail.com>
2023-03-13 07:53:57 -0600

    Agregue metodos para imprimir

--- Practica02/ExamenParcial.java
+++ Practica02/ExamenParcial.java
@@ -9,8 +9,8 @@ public class ExamenParcial {
     private static Stack<Integer> Pila = new Stack<Integer>();
     private static LinkedList<Integer> Cola1 = new LinkedList<Integer>();
     private static LinkedList<Integer> Cola2 = new LinkedList<Integer>();
-    int ElementoBorrado=0;
-    
+    int ElementoBorrado = 0;
+
     public class Nodo {
         int numcle;
         Nodo next;
@@ -27,7 +27,7 @@ public class ExamenParcial {
     Nodo nuevoNodo;
     int cantidad;
 
-    //METODO VACIA
+    // METODO VACIA
     private boolean VaciaPila() {
         return Pila.isEmpty();
     }// end Vacia Pila
@@ -40,7 +40,7 @@ public class ExamenParcial {
         return top == null;
     }// end vacia
 
-    //METODOS PARA INSERTAR
+    // METODOS PARA INSERTAR
     private void InsertarPila(int xelem) {
         Pila.push(xelem);
     }// end Insertar Pila
@@ -63,11 +63,12 @@ public class ExamenParcial {
         cantidad++;
     }// end Insertar elemento
 
-    //METODOS PARA BORRAR
+    // METODOS PARA BORRAR
     private void BorrarPila() {
         if (!VaciaPila()) {
             this.ElementoBorrado = Pila.pop();
-            //JOptionPane.showMessageDialog(null, "Elemento eliminado: " + ElementoBorrado);
+            // JOptionPane.showMessageDialog(null, "Elemento eliminado: " +
+            // ElementoBorrado);
         } else {
             JOptionPane.showMessageDialog(null, "La pila está vacía");
         } // end if else
@@ -76,9 +77,9 @@ public class ExamenParcial {
     private void BorrarCola(LinkedList<Integer> cola) {
         if (!VaciaCola(cola)) {
             this.ElementoBorrado = cola.poll();
-            JOptionPane.showMessageDialog(null,"Se elimino cliente: " + ElementoBorrado);
+            JOptionPane.showMessageDialog(null, "Se elimino cliente: " + ElementoBorrado);
         } else {
-            JOptionPane.showMessageDialog(null,"La cola esta vacia");
+            JOptionPane.showMessageDialog(null, "La cola esta vacia");
         } // end if else
     }// end Borrar Cola
 
@@ -98,7 +99,7 @@ public class ExamenParcial {
                 ant.next = null;
             } // borra dependiendo de si es el primero y deja la cola vacia o sino y borra el
               // ultimo
-            ElementoBorrado= temp.numcle;
+            ElementoBorrado = temp.numcle;
             JOptionPane.showMessageDialog(null, "Se ha borrado el empleado numero: " + ElementoBorrado);
             cantidad--;
         } else {
@@ -106,10 +107,10 @@ public class ExamenParcial {
         } // if else
     }// end Borrar
 
-    //METODOS PARA BORRAR DE UN LADO E INSERTAR EL OTRO LADO
+    // METODOS PARA BORRAR DE UN LADO E INSERTAR EL OTRO LADO
     private void BorrarColaInsertarPila() {
         BorrarCola(Cola1);
-        JOptionPane.showMessageDialog(null,"Se borro el elemento " + ElementoBorrado + " y se agrego a Pila");
+        JOptionPane.showMessageDialog(null, "Se borro el elemento " + ElementoBorrado + " y se agrego a Pila");
         InsertarPila(ElementoBorrado);
     }// end borrar cola e insertar pila
 
@@ -121,7 +122,7 @@ public class ExamenParcial {
             } else {
                 while (!Pila.peek().equals(xelem)) { // mientras que el ultimo elemtento no sea igual a xelem
                     BorrarPila(); // se borrara el elemento y se guarda en ElemtoBorrado
-                    InsertarNODO( ElementoBorrado); // se van insertando los ElementoBorrado a la Pila Temp
+                    InsertarNODO(ElementoBorrado); // se van insertando los ElementoBorrado a la Pila Temp
                 } // end while //
                 BorrarPila(); // Ya es el elemento que queriamos borrar y lo borramos
                 InsertarCola(Cola2, ElementoBorrado);// e inserta en cola
@@ -130,11 +131,48 @@ public class ExamenParcial {
                     InsertarPila(ElementoBorrado); // se van insertando los ElementoBorrado a la Pila
                 } // end whilw
             } // end ifn else
-            JOptionPane.showMessageDialog(null,"Se elimino de Pila y se agrego a Cola2 el cliente: " + ElementoBorrado);
+            JOptionPane.showMessageDialog(null,
+                    "Se elimino de Pila y se agrego a Cola2 el cliente: " + ElementoBorrado);
         } else {
-            JOptionPane.showMessageDialog(null,"La pila esta vacia");
-        }//end if else
+            JOptionPane.showMessageDialog(null, "La pila esta vacia");
+        } // end if else
     }// end Borrar pila e insertar cola
 
+    // METODO PARA IMPRIMIR
+    private void ImprimirPila() {
+        if (!VaciaPila()) {
+            JOptionPane.showMessageDialog(null, "Elementos de la pila: " + Pila);
+        } else {
+            JOptionPane.showMessageDialog(null, "La Pila esta vacia");
+        } // end if else
+    }// end imprimir pila
+
+    private void ImprimirCola(LinkedList<Integer> cola) {
+        if (!VaciaCola(cola)) {
+            JOptionPane.showMessageDialog(null, "Elementos de la cola: " + cola);
+        } else {
+            JOptionPane.showMessageDialog(null, "La Cola esta vacia");
+        } // end if else
+    }// end imprimir cola
+
+    private void TamanoGeneral() {
+        JOptionPane.showMessageDialog(null,"EL tamano de la pila es de: " + Pila.size()
+                + "\nEl tamano de Cola1 es de: " + Cola1.size()
+                + "\nEl tamano de Cola2 es de: " + Cola2.size());
+    }// end Tamabo general
+
+    private void PrimerElementoCola(    ) {
+        JOptionPane.showMessageDialog(null,"El primer elemento de la cola es: " + Cola1.getFirst());
+    }// end Primer elemento cola
     
+    private void UltimoElementoPila() {
+        JOptionPane.showMessageDialog(null,"EL ultimo elemento de la pla es: " + Pila.peek());
+    }// end ultimo elemento pila
+
+    private void UltimoElementoCola() {
+        JOptionPane.showMessageDialog(null,"El ultimo elemento de la cola es: " + Cola1.getLast());
+    }// end ultimo elemento cola
+
+    
+
 }// end class
