218272b595ddb0e79e8d96f0114b2450f42d1252
Coffe818 <josemanuelgrimaldovalero@gmail.com>
2023-03-27 14:42:38 -0600

    agregue un ejemplo

--- Tareas/RcursividadyNO.java
+++ Tareas/RcursividadyNO.java
@@ -6,7 +6,7 @@ public class RcursividadyNO {
     public static int FactorialRecursivo(int num) {
         if (num <= 1) {
             return 1;
-        }//end if
+        } // end if
         return num * FactorialRecursivo(num - 1);
 
     }// end Factorial
@@ -15,55 +15,63 @@ public class RcursividadyNO {
         int result = 1;
         for (int i = 1; i <= num; i++) {
             result *= i;
-        }//end for
+        } // end for
         return result;
     }// end Factorial NO recursivo
 
-    public static int FibnacciRecursico(int num){
+    public static int FibnacciRecursico(int num) {
         if (num == 0) {
             return 0;
-        }//end it
+        } // end it
         if (num == 1) {
             return 1;
-        }//end if
+        } // end if
         return FibnacciRecursico(num - 1) + FibnacciRecursico(num - 2);
-    }//end Fibonacci recursivo
+    }// end Fibonacci recursivo
 
-    public static int FibonacciNORecursivo(int num){
+    public static int FibonacciNORecursivo(int num) {
         if (num == 1) {
             return 1;
-        }//end if
+        } // end if
         int a = 0;
         int b = 1;
         for (int i = 2; i <= num; i++) {
             int c = a + b;
             a = b;
             b = c;
-        }//end for
+        } // end for
         return b;
-    }//end Fibonacci NO recursivo
+    }// end Fibonacci NO recursivo
 
-    public static int SeparaSumaNORecursivo(int num){
+    public static int SeparaSumaNORecursivo(int num) {
         int suma = 0;
         while (num != 0) {
             int digito = num % 10;
             suma += digito;
             num /= 10;
-        }//end while
+        } // end while
         return suma;
     }// end separa los digitos y los suma No Recursivo
 
-    public static int SeparaSumaRecursivo(int num){
+    public static int SeparaSumaRecursivo(int num) {
         if (num < 10) {
             return num;
         }
         return num % 10 + SeparaSumaRecursivo(num / 10);
-    }//end separa los digitos y los suma Recursivo
+    }// end separa los digitos y los suma Recursivo
+
+    /* Ejemplo de como funciona 
+     * SeparaSumaRecursivo(235)
+     * 5 + SeparaSumaRecursivo(23)
+     * 5 + (3 + SeparaSumaRecursivo(2))
+     * 5 + (3 + 2)
+     * 10 
+     */
 
     public static void main(String[] args) {
         int opcion;
         do {
-            opcion = Integer.parseInt(JOptionPane.showInputDialog(null, "======MENU RECURSIVIDAD===="+
+            opcion = Integer.parseInt(JOptionPane.showInputDialog(null, "======MENU RECURSIVIDAD====" +
                     "\n1. Factorial Recursivo" +
                     "\n2. Factorial NO Recursivo" +
                     "\n3. Fibonacci Recursivo" +
@@ -75,35 +83,41 @@ public class RcursividadyNO {
             switch (opcion) {
                 case 1:
                     num = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el numero: "));
-                    JOptionPane.showMessageDialog(null,"El numero factorial de " + num + " es: " + FactorialRecursivo(num));
+                    JOptionPane.showMessageDialog(null,
+                            "El numero factorial de " + num + " es: " + FactorialRecursivo(num));
                     break;
                 case 2:
                     num = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el numero: "));
-                    JOptionPane.showMessageDialog(null,"El numero factorial de " + num + " es: " + FacotrialNORecursivo(num));
+                    JOptionPane.showMessageDialog(null,
+                            "El numero factorial de " + num + " es: " + FacotrialNORecursivo(num));
                     break;
                 case 3:
                     num = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el numero: "));
-                    JOptionPane.showMessageDialog(null,"El numero fibonacci de " + num + " es: " + FibnacciRecursico(num));
+                    JOptionPane.showMessageDialog(null,
+                            "El numero fibonacci de " + num + " es: " + FibnacciRecursico(num));
                     break;
                 case 4:
                     num = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el numero: "));
-                    JOptionPane.showMessageDialog(null,"El numero fibonacci de " + num + " es: " + FibonacciNORecursivo(num));
+                    JOptionPane.showMessageDialog(null,
+                            "El numero fibonacci de " + num + " es: " + FibonacciNORecursivo(num));
                     break;
                 case 5:
                     num = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el numero: "));
-                    JOptionPane.showMessageDialog(null,"La suma de los digitos del "+num +" es de : " + SeparaSumaRecursivo(num));
+                    JOptionPane.showMessageDialog(null,
+                            "La suma de los digitos del " + num + " es de : " + SeparaSumaRecursivo(num));
                     break;
                 case 6:
                     num = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el numero: "));
-                    JOptionPane.showMessageDialog(null,"La suma de los digitos del "+num +" es de : " + SeparaSumaNORecursivo(num));
+                    JOptionPane.showMessageDialog(null,
+                            "La suma de los digitos del " + num + " es de : " + SeparaSumaNORecursivo(num));
                     break;
-                
+
                 case 0:
-                    JOptionPane.showMessageDialog(null,"Saliendo...");
+                    JOptionPane.showMessageDialog(null, "Saliendo...");
                     break;
 
                 default:
-                    JOptionPane.showMessageDialog(null,"Escoga una opcion valida");
+                    JOptionPane.showMessageDialog(null, "Escoga una opcion valida");
                     break;
             }// end switch
         } while (opcion != 0);
