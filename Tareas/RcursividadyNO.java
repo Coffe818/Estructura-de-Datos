f4c69234faacfed1a4d128a64188c8f99b0cb029
Coffe818 <josemanuelgrimaldovalero@gmail.com>
2023-03-27 08:52:40 -0600

    Agregue los fiboncacci

--- Tareas/RcursividadyNO.java
+++ Tareas/RcursividadyNO.java
@@ -17,26 +17,60 @@ public class RcursividadyNO {
             result *= i;
         }//end for
         return result;
-    }// end Factorial
+    }// end Factorial NO recursivo
+
+    public static int FibnacciRecursico(int num){
+        if (num == 0) {
+            return 0;
+        }//end it
+        if (num == 1) {
+            return 1;
+        }//end if
+        return FibnacciRecursico(num - 1) + FibnacciRecursico(num - 2);
+    }//end Fibonacci recursivo
+
+    public static int FibonacciNORecursivo(int num){
+        if (num == 1) {
+            return 1;
+        }//end if
+        int a = 0;
+        int b = 1;
+        for (int i = 2; i <= num; i++) {
+            int c = a + b;
+            a = b;
+            b = c;
+        }//end for
+        return b;
+    }//end Fibonacci NO recursivo
 
     public static void main(String[] args) {
         int opcion;
         do {
-            opcion = Integer.parseInt(JOptionPane.showInputDialog(null, "1. Factorial Recursivo" +
+            opcion = Integer.parseInt(JOptionPane.showInputDialog(null, "======MENU RECURSIVIDAD===="+
+                    "\n1. Factorial Recursivo" +
                     "\n2. Factorial NO Recursivo" +
+                    "\n3. Fibonacci Recursivo" +
+                    "\n4. Fibonacci NO Recursivo" +
                     "\n0. Salir"));
             int num;
             switch (opcion) {
                 case 1:
-
                     num = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el numero: "));
                     JOptionPane.showMessageDialog(null,"El numero factorial de " + num + " es: " + FactorialRecursivo(num));
                     break;
                 case 2:
-
                     num = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el numero: "));
                     JOptionPane.showMessageDialog(null,"El numero factorial de " + num + " es: " + FacotrialNORecursivo(num));
                     break;
+                case 3:
+                    num = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el numero: "));
+                    JOptionPane.showMessageDialog(null,"El numero fibonacci de " + num + " es: " + FibnacciRecursico(num));
+                    break;
+                case 4:
+                    num = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el numero: "));
+                    JOptionPane.showMessageDialog(null,"El numero fibonacci de " + num + " es: " + FibonacciNORecursivo(num));
+                    break;
+                
                 case 0:
                     JOptionPane.showMessageDialog(null,"Saliendo");
                     break;
