8a6534d6c71f551c7b47d706b1fad577ba89807d
Coffe818 <josemanuelgrimaldovalero@gmail.com>
2023-03-28 09:08:25 -0600

    Cree la clase

---
+++ Tareas/TorresDeHanoi.java
@@ -0,0 +1,13 @@
+package Tareas;
+
+import java.util.Stack;
+
+public class TorresDeHanoi {
+    private Stack<Integer> pila1 = new Stack<Integer>();
+    private Stack<Integer> pila2 = new Stack<Integer>();
+    private Stack<Integer> pila3 = new Stack<Integer>();
+
+
+
+
+}//end class
