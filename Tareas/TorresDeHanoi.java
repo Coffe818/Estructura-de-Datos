9770691873e0e600f791c965d71de8d9f7cd0f78
Coffe818 <josemanuelgrimaldovalero@gmail.com>
2023-03-28 20:48:53 -0600

    agregue su main

--- Tareas/TorresDeHanoi.java
+++ Tareas/TorresDeHanoi.java
@@ -35,8 +35,46 @@ public class TorresDeHanoi {
             System.out.println("Movimiento no válido");
         }//end if else
     }// end Mover Disco
+
     
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese la cantidad de discos: ");
+        int n = sc.nextInt();
+
+        for (int i = n; i >= 1; i--) {
+            pila1.push(i);
+        }
+        boolean juegoTerminado = false;
+        while (!juegoTerminado) {
+            System.out.println("Pila A: " + pila1.toString());
+            System.out.println("Pila B: " + pila2.toString());
+            System.out.println("Pila C: " + pila3.toString());
+            System.out.print("Ingrese la pila de origen (1, 2, 3): ");
+            origen = sc.next().charAt(0);
+            System.out.print("Ingrese la pila de destino (1, 2, 3): ");
+            destino = sc.next().charAt(0);
+            switch (origen) {
+                case 1:
+                    MoverDisco(pila1, destino, n);
+                    break;
+                case 2:
+                    MoverDisco(pila2, destino, n);
+                    break;
+                case 3:
+                    MoverDisco(pila3, destino, n);
+                    break;
+                default:
+                    System.out.println("Pila no válida");
+                    break;
+            }
+            if (pila3.size() == n) {
+                System.out.println("¡Juego terminado!");
+                juegoTerminado = true;
+            }
+        }
+        sc.close();
+    }
 
     
 }// end class
