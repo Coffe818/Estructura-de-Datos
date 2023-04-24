ad5aeab0e3b088f2695a3a20acba23c1f173bf59
Coffe818 <josemanuelgrimaldovalero@gmail.com>
2023-04-24 08:48:51 -0600

    Agregue ObtenerSucasor(), Eliminar(), BajaFisica()

--- Tareas/ArbolesBinarios.java
+++ Tareas/ArbolesBinarios.java
@@ -58,5 +58,43 @@ public class ArbolesBinarios {
                 actual = actual.Hijoder;
             }//end if else que se recorre hasta encontrar el que queremos
         }//end while
-    }
+    }//end Baja Logica
+
+    private Nodo ObtenerSucesor(Nodo actual) {
+        while (actual.Hijoizq != null) {
+            actual = actual.Hijoizq;
+        }//while para posicionar
+        return actual;
+    }//end Obtener Sucesor
+
+    private Nodo Eliminar(Nodo actual, int matricula) {//donde inicia , que matricula eliminar
+        if (actual == null) {
+            return null;
+        }
+
+        if (matricula == actual.matricula) {
+            if (actual.Hijoizq == null) {
+                return actual.Hijoder;
+            } else if (actual.Hijoder == null) {
+                return actual.Hijoizq;
+            } else {
+                Nodo sucesor = ObtenerSucesor(actual.Hijoder);
+                actual.matricula = sucesor.matricula;
+                actual.nombre = sucesor.nombre;
+                actual.Hijoder = Eliminar(actual.Hijoder, sucesor.matricula);
+                return actual;
+            }//regresa actual que queremos eliminar
+        } else if (matricula < actual.matricula) {
+            actual.Hijoizq = Eliminar(actual.Hijoizq, matricula);
+            return actual;
+        } else {
+            actual.Hijoder = Eliminar(actual.Hijoder, matricula);
+            return actual;
+        }//recursividad hasta encontrar el nodo que queremos eliminar
+    }// end Eliminar
+
+    public void BajaFisica(int matricula) {
+        raiz = Eliminar(raiz, matricula);
+    }// Baja Fisica
+    
 }// end calss
